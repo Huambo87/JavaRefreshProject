456241d2b42f45a36c99995f6bdd08ce43f6e955
Huambo87 <maximgladkov@hotmail.com>
2024-04-18 16:10:51 -0400

    My first commit

---
+++ src/main/java/lesson1/Lesson1Main.java
@@ -0,0 +1,34 @@
+package lesson1;
+
+import javax.sound.midi.Soundbank;
+
+public class Lesson1Main {
+    public static void main(String[] args) {
+
+        String txt = "Hello, Max!";
+        int num = 112;
+        double fraction = 112.5;
+        long longNumber = 287654321;
+        boolean boolean1 = true;
+        boolean boolean2 = false;
+
+        int a = 100;
+        int b = 200;
+        double result = (double) a/b;
+
+
+//        System.out.println(txt);
+//        System.out.println("Ny number is " + num);
+//        System.out.println("My fraction is " + fraction);
+//
+//        System.out.println("BooleanVar1 is " + boolean1);
+//        System.out.println("BooleanVar2 is " + boolean2);
+
+        System.out.println("My result is " + result);
+
+
+    }
+}
+
+
+
