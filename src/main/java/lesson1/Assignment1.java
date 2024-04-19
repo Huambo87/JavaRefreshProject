f21219769fa2b2addbe7396c0bab9fbf7b3e1ff1
Huambo87 <maximgladkov@hotmail.com>
2024-04-19 14:13:21 -0400

    First commit to new branch

---
+++ src/main/java/lesson1/Assignment1.java
@@ -0,0 +1,29 @@
+package lesson1;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+
+        String intro = "Hello, Java!";
+        String myName = "Maxim Gladkov";
+
+        System.out.println(intro);
+        System.out.println(myName);
+
+        int var1 = -5 + 8 * 6;
+        double var2 = 20 + -3*5 / 8;
+
+        System.out.println(var1);
+        System.out.println(var2);
+
+        double a = 1.2;
+        double b = 1.1;
+        double c = 1.5;
+        double d = 1.6;
+        double e = 3.62;
+        double result = a*b*c*d*e;
+
+        System.out.println("number a * number b * number c * number d * number e equals " + result);
+
+    }
+
+}
