389a5e4c9f549312a4b46c8ff42cc4f2b21c2c9f
Huambo87 <maximgladkov@hotmail.com>
2024-04-19 15:41:17 -0400

    New class added, existing class modified

--- src/main/java/lesson1/Assignment2.java
+++ src/main/java/lesson1/Assignment2.java
@@ -1,4 +1,14 @@
 package lesson1;
 
 public class Assignment2 {
+    public static void main(String[] args) {
+
+        int var1 = -8 + 10 * 6;
+        double var2 = 20 + -3*5 / 8.2;
+
+        System.out.println(var1);
+        System.out.println(var2);
+
+    }
+
 }
